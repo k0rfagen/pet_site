a413b26cb93d590fa6350b40aa14000d9785176f
k0rfagen <atopolev2910@gmail.com>
2025-02-28 21:23:47 +0300

    добавлен класс css  search-text для формы ввода поиска

--- shop/forms.py
+++ shop/forms.py
@@ -2,4 +2,7 @@ from django import forms
 from .models import *
 
 class SearchForm(forms.Form):
-    search_body = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Поиск...'}))
+    search_body = forms.CharField(label='', widget=forms.TextInput(attrs={
+        'placeholder': 'Поиск...',
+        'class': 'search-text',
+         }))
