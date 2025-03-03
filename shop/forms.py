625b4f1948fa78df06aa5c12cc1dab60e79bbe01
k0rfagen <atopolev2910@gmail.com>
2025-03-03 13:07:07 +0300

    добавлена функция очистки корзины, но пока работает неправильно

--- shop/forms.py
+++ shop/forms.py
@@ -5,4 +5,5 @@ class SearchForm(forms.Form):
     search_body = forms.CharField(label='', widget=forms.TextInput(attrs={
         'placeholder': 'Поиск...',
         'class': 'search-text',
+
          }))
