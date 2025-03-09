d87528beccd740711287b47876d3a08419308bc2
Булат Палушев <bfpalushev@gmail.com>
2025-03-09 21:17:33 +0300

    Добавил подсказки к поиску с помощью js, вроде стилизовал их

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,4 +1,27 @@
 function searchTips() {
     const input = document.querySelector('.search-text').value;
-    document.querySelector('.search-tips').innerHTML = input;
+    if (input !== '') {
+        const items = document.querySelectorAll('.item');
+        let finalItems = [];
+        for (let i = 0; i < items.length; i++) {
+            const name = items[i].querySelector('h1').innerText;
+            if (name.toLowerCase().includes(input.toLowerCase())) {
+                finalItems.push(name);
+            }
+        }
+        console.log(finalItems[0]);
+        if (finalItems[0]) {
+            document.querySelector('.tip1').innerText = finalItems[0];
+        }
+        if (finalItems[1]) {
+            document.querySelector('.tip2').innerText = finalItems[1];
+        }
+        if (finalItems[2]) {
+            document.querySelector('.tip3').innerText = finalItems[2];
+        }
+    } else {
+        document.querySelector('.tip1').innerText = '';
+        document.querySelector('.tip2').innerText = '';
+        document.querySelector('.tip3').innerText = '';
+    }
 }
